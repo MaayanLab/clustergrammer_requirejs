f4fdfc8d2004ed4474988df51e202d4e73f8b497
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-03-03 15:09:54 -0500

    have leaflet.js loading, but no map is showing

---
+++ app.js
@@ -0,0 +1,26 @@
+require.config({
+    paths: {
+      'leaflet': 'https://unpkg.com/leaflet@1.0.2/dist/leaflet'
+       },
+    shim: {
+       leaflet: {
+          exports: 'L'
+           },
+        } })
+
+require(['leaflet'], function (L) {
+  seattle = {lat: 47.59,    long: -122.335167, zoom: 12};
+  denver  = {lat: 39.74739, long: -105,        zoom: 13};
+  loc = seattle;
+
+  var map = L.map('mapDiv').setView([loc.lat, loc.long], loc.zoom);
+  var mapURI = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw';
+
+  L.tileLayer(mapURI, {
+             maxZoom: 18,
+             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                           '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                           'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+             id: 'mapbox.light'
+     }).addTo(map);
+  }); // require
